851793bb4557ecf6e776dd0663ed51ffb9e783d2
vidunithaedirisooriya <vidunithaedirisooriya@gmail.com>
2025-12-23 09:08:47 +0530

    replacing BTclassic with BLE for mobile battery friendliness and app convenience

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #include <IRremote.hpp>
 #include <Wire.h>
 #include <U8g2lib.h>
-#include "BluetoothSerial.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -15,13 +18,36 @@
 #endif
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-#define DECODE_NEC                                    //comment this line if you use other protocols
+#define DECODE_NEC                                    //comment this line and add your protocol if you use a remote with other IR protocols
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-BluetoothSerial ESP_BT;
+#define bleServerName   "venturo 0001"
+#define SERVICE_UUID    "5cfd3a85-6b69-4396-85e3-bdef0b414d0a"
+#define relayState_UUID "5cfd3a86-6b69-4396-85e3-bdef0b414d0a"
+#define timeSrc_UUID    "5cfd3a87-6b69-4396-85e3-bdef0b414d0a"
+#define n_UUID          "5cfd3a88-6b69-4396-85e3-bdef0b414d0a"
+#define timer_UUID      "5cfd3a89-6b69-4396-85e3-bdef0b414d0a"
+#define swing_UUID      "5cfd3a8a-6b69-4396-85e3-bdef0b414d0a"
+#define name_UUID       "5cfd3a8b-6b69-4396-85e3-bdef0b414d0a"
 
+BLECharacteristic relayStateCharacteristic(BLEUUID(relayState_UUID), BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE_NR);
+BLEDescriptor relayStateDescriptor(BLEUUID(relayState_UUID));
+
+BLECharacteristic timeSrcCharacteristic(BLEUUID(timeSrc_UUID), BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE_NR);
+BLEDescriptor timeSrcDescriptor(BLEUUID(timeSrc_UUID));
+
+BLECharacteristic nCharacteristic(BLEUUID(n_UUID), BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE_NR);
+BLEDescriptor nDescriptor(BLEUUID(n_UUID));
+
+BLECharacteristic swingCharacteristic(BLEUUID(swing_UUID), BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE_NR);
+BLEDescriptor swingDescriptor(BLEUUID(swing_UUID));
+
+BLECharacteristic timerCharacteristic(BLEUUID(timer_UUID), BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE_NR);
+BLEDescriptor timerDescriptor(BLEUUID(timer_UUID));
+
+BLECharacteristic nameCharacteristic(BLEUUID(name_UUID), BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE);
+BLEDescriptor nameDescriptor(BLEUUID(name_UUID));
+
+bool deviceConnected = false;
 
 unsigned long currentMillis;              //variables to store the time for incoming timing functions
 unsigned long prevOnMillis;
@@ -33,14 +59,13 @@ unsigned long prevToggleSwing;
 unsigned long prevCountDownMillis;
 
 bool relayState;      //power: on/off var
-int n =0;                //speed
+int n =0;                //speed of rotation of fan motor
 int timeSrc =0;          //number of minutes in timer
 bool timer;           //timer: on/off
 bool swing;           //swinging or not
 
-int speeds[]={23,18,5,4};   //pins to which the speed controlling relays are connected
+int speeds[]={23,18,5,4};   //pins to which the speed controlling modules (triacs) are connected
 
-int incoming;         //to store BT message
 
 const unsigned char hourglass [] PROGMEM = {        //hourglas icon bitmap
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0xff, 0xff, 0x0f, 
@@ -63,10 +88,69 @@ const unsigned char nothing [] PROGMEM = {
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
+// Setup callbacks onConnect and onDisconnect (BLE) 
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+    Serial.println("Device Connected");
+  };
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+    Serial.println("Device Disconnected");
+    pServer->getAdvertising()->start();
+  }
+};
+
+class AllCallbacks: public BLECharacteristicCallbacks { //handle controlling this device via BLE app
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string value = pCharacteristic->getValue();
+    uint16_t numValue = *(uint16_t*)value.data();
+    // Serial.print(numValue);
+    // Serial.print("__from__");
+    // Serial.println(pCharacteristic->getUUID().toString().c_str());
+    // Check which characteristic this is
+    if (pCharacteristic->getUUID().equals(BLEUUID(relayState_UUID))) {
+      // Handle RelayState
+      relayState=!relayState;
+      uint8_t relayStateForBLE = relayState ? 1 : 0;
+      relayStateCharacteristic.setValue(&relayStateForBLE,1);
+      relayStateCharacteristic.notify();
+    }
+    else if (pCharacteristic->getUUID().equals(BLEUUID(timeSrc_UUID))) {
+      timeSrc = numValue;
+      timeSrcCharacteristic.setValue(timeSrc);
+      timeSrcCharacteristic.notify(); 
+    }
+    else if (pCharacteristic->getUUID().equals(BLEUUID(n_UUID))) {
+      // Handle N
+      n = numValue ;
+      nCharacteristic.setValue(n);
+      nCharacteristic.notify(); 
+    }
+    else if (pCharacteristic->getUUID().equals(BLEUUID(timer_UUID))) {
+      // Handle N
+      timer=!timer;
+      uint8_t timerValueForBLE = timer ? 1 : 0;
+      timerCharacteristic.setValue(&timerValueForBLE, 1);
+      timerCharacteristic.notify();
+    }
+    else if (pCharacteristic->getUUID().equals(BLEUUID(swing_UUID))) {
+      // Handle N
+      swing=!swing;
+      uint8_t swingValueForBLE = swing ? 1 : 0;
+      swingCharacteristic.setValue(&swingValueForBLE, 1);
+      swingCharacteristic.notify();
+    }
+    // else if (pCharacteristic->getUUID().equals(BLEUUID(name_UUID))) {
+    //   // Handle N
+    //   if (numValue == 1) Serial.println("hey i got the command to power toggle");
+    // }
+  }
+};
+
 void setup() {
   Serial.begin(9600);
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  ESP_BT.begin("fan_ESP32");
   pinMode(13, INPUT);
   pinMode(12, INPUT);
   pinMode(14, INPUT);
@@ -82,14 +166,47 @@ void setup() {
   digitalWrite(4, false);
 
   u8g2.begin();
-  delay(500);
+  BLEDevice::init(bleServerName);
+  
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  
+  // Create the BLE Service
+  BLEService *fanService = pServer->createService(SERVICE_UUID);
+  
+  // Create BLE Characteristics and corresponding Descriptors
+  fanService->addCharacteristic(&relayStateCharacteristic);
+  relayStateCharacteristic.setCallbacks(new AllCallbacks());
+  
+  fanService->addCharacteristic(&timeSrcCharacteristic);
+  timeSrcCharacteristic.setCallbacks(new AllCallbacks());
+  
+  fanService->addCharacteristic(&nCharacteristic);
+  nCharacteristic.setCallbacks(new AllCallbacks());
+  
+  fanService->addCharacteristic(&swingCharacteristic);
+  swingCharacteristic.setCallbacks(new AllCallbacks());
+  
+  fanService->addCharacteristic(&timerCharacteristic);
+  timerCharacteristic.setCallbacks(new AllCallbacks());
+  
+  fanService->addCharacteristic(&nameCharacteristic);
+  nameCharacteristic.setCallbacks(new AllCallbacks());
+  
+  fanService->start();
+  
+  pServer->getAdvertising()->start();
+  delay(50);
 }
 
 void changerelayState() {   //function to toggle power when called
   if (currentMillis - prevOnMillis >= 1250) {
     prevOnMillis = currentMillis;
     relayState = !relayState;
-    ESP_BT.write(102 - relayState);
+    uint8_t relayStateForBLE = relayState ? 1 : 0;
+    relayStateCharacteristic.setValue(&relayStateForBLE,1);
+    relayStateCharacteristic.notify();
   }
 }
 
@@ -97,46 +214,49 @@ void changeSpeed() {      //function to change speed when called
   if (currentMillis-prevSpeedMillis >= 250) {
     prevSpeedMillis = currentMillis;
     n++;
-    if(n > 3){ n = 0; }
-    ESP_BT.write(104 + n);
+    if(n > 3) n = 0;
+    nCharacteristic.setValue(n);
+    nCharacteristic.notify();
   }
 }
 
 void incrementTimeSrc() {   //function to increase timer amount when called by IR or touch
- if(currentMillis - prevIncrementTimeSrc >= 50){
-  prevIncrementTimeSrc = currentMillis;
-  timeSrc++;
-  ESP_BT.write(timeSrc);
- }
- if(timeSrc > 99){
-  timeSrc=99;
- }
+  if(currentMillis - prevIncrementTimeSrc >= 50){
+    prevIncrementTimeSrc = currentMillis;
+    timeSrc++;
+  }
+  if(timeSrc > 99) timeSrc=99;
+  timeSrcCharacteristic.setValue(timeSrc);
+  timeSrcCharacteristic.notify();
 }
 
 void decrementTimeSrc() {   //function to decrease timer amount when called by IR or touch
   if(currentMillis - prevDecrementTimeSrc >=50){
     prevDecrementTimeSrc = currentMillis;
     timeSrc = timeSrc-1;
-    ESP_BT.write(timeSrc);
   }
-  if(timeSrc < 0) {
-    timeSrc=0;
-  }
+  if(timeSrc < 0) timeSrc=0;
+  timeSrcCharacteristic.setValue(timeSrc);
+  timeSrcCharacteristic.notify();
 }
 
 void toggleTimer() {        //turn timer on/off
   if(currentMillis - prevToggleTimer >= 250){
     prevToggleTimer = currentMillis;
-   timer = !timer;
-   ESP_BT.write(112 + timer);
+    timer = !timer;
+    uint8_t timerValueForBLE = timer ? 1 : 0;
+    timerCharacteristic.setValue(&timerValueForBLE, 1);
+    timerCharacteristic.notify();
   }
 }
 
 void toggleSwing() {        //same old
   if(currentMillis - prevToggleSwing >= 250){
     prevToggleSwing = currentMillis;
-   swing = !swing;
-   ESP_BT.write(109 + timer);
+    swing = !swing;
+    uint8_t swingValueForBLE = swing ? 1 : 0;
+    swingCharacteristic.setValue(&swingValueForBLE, 1);
+    swingCharacteristic.notify();
   }
 }
 
@@ -193,61 +313,42 @@ void ctrlPanel(){     //read what touchbuttons are contacted and function accord
   }
 }
 
-void BTctrl() {       //input bluetooth functions and update app display
-  if (ESP_BT.available()) {
-    incoming = ESP_BT.read();
-    switch (incoming) {
-      case 100:
-        relayState = !relayState;
-        ESP_BT.write(102 - relayState);   //update app that it changed succesfully
-       break;
-      case 103:
-        if (n < 3) {
-          n++;
-        } else {
-          n = 0;
-        }
-        ESP_BT.write(104 + n);
-       break;
-      case 108:
-        swing = !swing;
-        ESP_BT.write(109 + swing);
-       break;
-      case 111:
-        timer = !timer;
-        ESP_BT.write(112 + timer);
-       break;
-      case 0 ... 99:
-        timeSrc = incoming;
-      break;
-    }
-  }
-}
+
 
 void execute() {      //timer countdown, display and misc.
  digitalWrite(speeds[n],relayState);
- Serial.println(n);
- Serial.println(relayState);
- Serial.println("on");
+//  Serial.println(n);
+//  Serial.println(relayState);
+//  Serial.println("on");     //these serial.prints were used for troubleshooting
  digitalWrite(speeds[n-1], LOW);
- Serial.println(n-1);
- if(n!=3){digitalWrite(speeds[3], LOW);}
-digitalWrite(25, swing);     //the relays turns on when 0 and off when 1 (this is because most relay modules I bought are this way.)
+//  Serial.println(n-1);
+ if(n!=3) digitalWrite(speeds[3], LOW);   //the relays turns on when 0 and off when 1 (this is because most relay modules I bought are this way.)
+ digitalWrite(25, swing&&relayState);   //let the fan swing when it is spinning and commanded to swing  
 
  if(timer == 1) {
-  if(currentMillis - prevCountDownMillis >= 60000){
-    prevCountDownMillis = currentMillis;
-    timeSrc = timeSrc - 1;
-    if(timeSrc <= 0){
-      timeSrc = 0;
+      if(currentMillis - prevCountDownMillis >= 60000){
+        prevCountDownMillis = currentMillis;
+        timeSrc = timeSrc - 1;
+        if(timeSrc <= 0) timeSrc = 0;
+        timeSrcCharacteristic.setValue(timeSrc);
+        timeSrcCharacteristic.notify();
+      }
+    if(timeSrc == 0){
+      relayState = 0;
+      timer = 0;
+
+      timeSrcCharacteristic.setValue(timeSrc);
+      timeSrcCharacteristic.notify();
+
+      uint8_t relayStateForBLE = relayState ? 1 : 0;
+      relayStateCharacteristic.setValue(&relayStateForBLE,1);
+      relayStateCharacteristic.notify();
+
+      uint8_t timerValueForBLE = timer ? 1 : 0;
+      timerCharacteristic.setValue(&timerValueForBLE, 1);
+      timerCharacteristic.notify();
     }
   }
-  if(timeSrc == 0){
-    relayState = 0;
-    timer = 0;
-  }
-  ESP_BT.write(timeSrc);    //update BT app indicator
- }
 
   u8g2.firstPage();
   do{
@@ -275,8 +376,7 @@ void loop() {     //just call the functions
   remote();
   ctrlPanel();
   execute();
-  BTctrl();
-
+  delay(50);
 }
  
 //vidunithaedirisooriya 2025
